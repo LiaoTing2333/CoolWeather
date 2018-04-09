bf6040e6e2e1d6d6216a4aa2ee2d0542a060021f
LiaoTing <2632922126@qq.com>
2018-04-09 18:57:24 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,12 +1,16 @@
 package com.coolweather.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 不闻不问不听不看不在乎~ on 2018/4/8.
+ */
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
